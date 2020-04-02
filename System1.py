34c5b46a3930a9ce3cffe25608bcb3ec51a6f073
Bright Gee Varghese <brightlsom@gmail.com>
2020-04-02 17:04:57 +0530

    TCP Server program
    Run this program(System1.py) first.

---
+++ System1.py
@@ -0,0 +1,20 @@
+import socket
+
+# create a socket object
+s = socket.socket()
+# Associate socket object with a port number (0 to 65535)
+# to reserve a port number for our service
+s.bind(('127.0.0.1', 2500))
+# keep our system in listening mode
+s.listen()
+while True:
+    # wait for connection request
+    mySocket, addressOfOtherSystem = s.accept()
+    print('Connection accepted from '+str(addressOfOtherSystem))
+    # receive data from system-2
+    msg = mySocket.recv(100)
+    print(msg.decode('utf-8'))
+    # send welcome msg to system-2
+    mySocket.send(bytes('welcome', 'utf-8'))
+    # close mySocket
+    mySocket.close()
